a43536b9a6a74bc89da2e74f256857d4f1ea3c16
Roshni Srikanth <rsrika@uw.edu>
2023-10-25 14:57:47 -0700

    adding reflection content to page after fetching

--- src/js/quarterlyReflection.js
+++ src/js/quarterlyReflection.js
@@ -17,6 +17,7 @@ export function QuarterlyReflection(props) {
     let urlParams = useParams();
     let reflectionContentYear = urlParams.year; 
     let reflectionContentQuarter = urlParams.quarter;
+    let quarterImageName = reflectionContentQuarter+"Image";
     
     let year =  props.data;
     year = year.filter((entry) => {
@@ -30,6 +31,18 @@ export function QuarterlyReflection(props) {
         fetch(year.reflections) 
             .then(response => response.json())
             .then(data => {
+                if(reflectionContentQuarter === 'autumn') {
+                    data = data[0];
+                }
+                else if(reflectionContentQuarter === 'winter'){
+                    data = data[1];
+                }
+                else if(reflectionContentQuarter === 'spring'){
+                    data = data[2];
+                }
+                else {
+                    data = data[3];
+                }
                 setQuarterData(data);
                 console.log("Quarterly data:" , data);
             })
@@ -52,6 +65,30 @@ export function QuarterlyReflection(props) {
                     <span onClick= {() => {props.handleClickYear(year.Year);}}>&nbsp;{reflectionContentQuarter.toUpperCase()}</span>
                 </h1>
             </div>
+            <div className="spacer"/>
+            
+            <div className="container d-flex">
+                <img className="img-fluid mx-auto"src={year[quarterImageName]} alt="img1"/> 
+            </div>
+            <div className="half-spacer"/>
+            <div className="quarterReflection container d-flex">
+                <div className='col'>
+                    <div className='row d-flex'>
+                        <strong className="cursive larger-letters mx-auto">{reflectionContentQuarter + " quarter reflection"}</strong>
+                    </div>
+                    <div className='half-spacer'/>
+                    <div className='row'>
+                        <p className="intro">
+                            {quarterData.reflection}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="half-spacer"/>
+            <div className="quarterArtifacts container d-flex">
+                
+            </div>
+           
 
             <div className="spacer"></div>
             <div className="spacer"></div>
